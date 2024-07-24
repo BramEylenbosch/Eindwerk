a927e2cd50dab400bfcd9aff0c2b75268fc64e4d
BramEylenbosch <bram.eylenbosch@student.ehb.be>
2024-07-24 13:14:37 +0200

    Initial Commit

---
+++ Eindwerk/EindwerkApp.swift
@@ -0,0 +1,17 @@
+//
+//  EindwerkApp.swift
+//  Eindwerk
+//
+//  Created by Bram on 24/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct EindwerkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
